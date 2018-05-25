07e6d3d8fe6a115da48cec1a6ba078edaff85e5d
François MATHIEU <maps_red@yahoo.fr>
2018-05-25 16:43:33 +0200

    Init Project

---
+++ TicketingBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fma
+ * Date: 25/05/18
+ * Time: 16:41
+ */
+
+namespace Maps_red\TicketingBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TicketingBundle extends Bundle
+{
+
+}
