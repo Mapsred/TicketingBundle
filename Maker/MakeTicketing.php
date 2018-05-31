54d03f62fcaff5f0f546678d14941712623a0605
François Mathieu <maps_red@yahoo.fr>
2018-06-01 00:56:49 +0200

    Resolve #7 | Add MakeTicket command

---
+++ Maker/MakeTicketing.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Maps_red\TicketingBundle\Maker;
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\ORM\Mapping\Column;
+use Maps_red\TicketingBundle\Entity\Ticket;
+use Symfony\Bundle\MakerBundle\ConsoleStyle;
+use Symfony\Bundle\MakerBundle\DependencyBuilder;
+use Symfony\Bundle\MakerBundle\Generator;
+use Symfony\Bundle\MakerBundle\InputConfiguration;
+use Symfony\Bundle\MakerBundle\Maker\AbstractMaker;
+use Symfony\Bundle\MakerBundle\Util\ClassNameDetails;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Bundle\MakerBundle\FileManager;
+
+class MakeTicketing extends AbstractMaker
+{
+    /**
+     * @var FileManager
+     */
+    private $fileManager;
+
+    public function __construct(FileManager $fileManager)
+    {
+        $this->fileManager = $fileManager;
+    }
+
+    /**
+     * Return the command name for your maker (e.g. make:report).
+     *
+     * @return string
+     */
+    public static function getCommandName(): string
+    {
+        return "make:ticketing";
+    }
+
+    /**
+     * @param Command $command
+     * @param InputConfiguration $inputConfig
+     */
+    public function configureCommand(Command $command, InputConfiguration $inputConfig)
+    {
+        $command
+            ->setDescription('Create the entities and repositories')
+            ->setHelp("The <info>%command.name%</info> command generates the Ticket entities.")
+            ->addOption('regenerate', null, InputOption::VALUE_NONE, 'Simply generate the class');
+    }
+
+    /**
+     * Called after normal code generation: allows you to do anything.
+     *
+     * @param InputInterface $input
+     * @param ConsoleStyle $io
+     * @param Generator $generator
+     */
+    public function generate(InputInterface $input, ConsoleStyle $io, Generator $generator)
+    {
+        $classes = [Ticket::class];
+        foreach ($classes as $classFQN) {
+            $regenerate = $input->getOption('regenerate');
+            $className = substr($classFQN, strrpos($classFQN, '\\') + 1);
+            $entityClassDetails = $generator->createClassNameDetails($className, 'Entity\\');
+
+            $repositoryClassDetails = $generator->createClassNameDetails(
+                $entityClassDetails->getRelativeName(),
+                'Repository\\',
+                'Repository'
+            );
+
+            $this->remove($entityClassDetails, $io, $regenerate);
+            $this->remove($repositoryClassDetails, $io, $regenerate);
+
+            if (!class_exists($entityClassDetails->getFullName()) || $regenerate) {
+                $this->generateEntity($generator, $entityClassDetails, $repositoryClassDetails, $className, $classFQN);
+            }
+
+            if (!class_exists($repositoryClassDetails->getFullName()) || $regenerate) {
+                $this->generateRepository($generator, $entityClassDetails, $repositoryClassDetails, $className, $classFQN);
+            }
+
+            $generator->writeChanges();
+
+        }
+    }
+
+    private function remove(ClassNameDetails $classDetails, ConsoleStyle $io, bool $regenerate)
+    {
+        if (class_exists($classDetails->getFullName()) && $regenerate) {
+            if (null !== $targetPath = $this->fileManager->getRelativePathForFutureClass($classDetails->getFullName())) {
+                unlink($targetPath);
+                $io->comment(sprintf('<fg=red>%s</>: %s', "removed", $targetPath));
+            }
+        }
+
+    }
+
+    /**
+     * @param Generator $generator
+     * @param ClassNameDetails $entityClassDetails
+     * @param ClassNameDetails $repositoryClassDetails
+     * @param $className
+     * @param $classFQN
+     */
+    private function generateEntity(Generator $generator, ClassNameDetails $entityClassDetails, ClassNameDetails $repositoryClassDetails, $className, $classFQN)
+    {
+        $generator->generateClass(
+            $entityClassDetails->getFullName(),
+            __DIR__ . '/../Resources/skeleton/doctrine/Entity.tpl.php',
+            [
+                'repository_full_class_name' => $repositoryClassDetails->getFullName(),
+                'parent_name' => $className,
+                'parent_namespace' => $classFQN,
+            ]
+        );
+
+    }
+
+    /**
+     * @param Generator $generator
+     * @param ClassNameDetails $entityClassDetails
+     * @param ClassNameDetails $repositoryClassDetails
+     */
+    private function generateRepository(Generator $generator, ClassNameDetails $entityClassDetails, ClassNameDetails $repositoryClassDetails)
+    {
+        $entityAlias = strtolower($entityClassDetails->getShortName()[0]);
+        $generator->generateClass(
+            $repositoryClassDetails->getFullName(),
+            'doctrine/Repository.tpl.php',
+            [
+                'entity_full_class_name' => $entityClassDetails->getFullName(),
+                'entity_class_name' => $entityClassDetails->getShortName(),
+                'entity_alias' => $entityAlias,
+            ]
+        );
+    }
+
+    /**
+     * Configure any library dependencies that your maker requires.
+     *
+     * @param DependencyBuilder $dependencies
+     */
+    public function configureDependencies(DependencyBuilder $dependencies)
+    {
+        // guarantee DoctrineBundle
+        $dependencies->addClassDependency(DoctrineBundle::class, 'orm');
+
+        // guarantee ORM
+        $dependencies->addClassDependency(Column::class, 'orm');
+    }
+}
---
+++ Resources/skeleton/doctrine/Entity.tpl.php
@@ -0,0 +1,24 @@
+<?= "<?php\n" ?>
+
+namespace <?= $namespace ?>;
+
+use Doctrine\ORM\Mapping as ORM;
+use <?= $parent_namespace ." as Base" .$parent_name.";\n"?>
+
+/**
+ * @ORM\Entity(repositoryClass="<?= $repository_full_class_name ?>")
+ */
+class <?= $class_name ?> extends <?= "Base".$parent_name ."\n"?>
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
