d46a83d55929cf7faea0716421b984823df1f266
François Mathieu <maps_red@yahoo.fr>
2018-06-01 19:17:08 +0200

    Update #8 | Add config for history

--- Manager/TicketManager.php
+++ Manager/TicketManager.php
@@ -10,15 +10,20 @@ class TicketManager extends AbstractManager
     /** @var string $defaultStatusName */
     private $defaultStatusName;
 
+    /** @var bool $enableHistory */
+    private $enableHistory;
+
     /**
      * TicketManager constructor.
      * @param EntityManagerInterface $manager
      * @param string $class
      * @param string $defaultStatusName
+     * @param bool $enableHistory
      */
-    public function __construct(EntityManagerInterface $manager, string $class, string $defaultStatusName)
+    public function __construct(EntityManagerInterface $manager, string $class, string $defaultStatusName, bool $enableHistory)
     {
         $this->defaultStatusName = $defaultStatusName;
+        $this->enableHistory = $enableHistory;
         parent::__construct($manager, $class);
     }
 }
